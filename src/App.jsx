3ee0bcbd7170f7074bdbf59c5dcc2fa66e55bcce
Ali-Hasan6 <ali.hasan@students.auk.edu.krd>
2023-09-28 21:21:55 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return <div>
+    <div className=" w-screen h-screen flex justify-center items-center">
+      <div className=" w-[50%] h-[50%] bg-[#D3D0C3]/90 rounded-md flex justify-center items-center ">
+        <a className=" hover:cursor-pointer hover:scale-105 duration-200 hover:" href="tel:07510508670">
+          <p>Number: 07510508670</p>
+        </a>
+        <p></p>
+      </div>
+    </div>
+  </div>;
+}
+
+export default App;
